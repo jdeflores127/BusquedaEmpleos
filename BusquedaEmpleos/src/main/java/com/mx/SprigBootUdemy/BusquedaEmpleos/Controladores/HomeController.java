f94754edfe15e38e64e8e890fda6a99df4161e3c
Ale Degollado <jairdego27@yahoo.com.mx>
2020-01-23 20:49:15 -0600

    Se crea html para ver listado de carreras. Se itera con each para mostrarlas

--- BusquedaEmpleos/src/main/java/com/mx/SprigBootUdemy/BusquedaEmpleos/Controladores/HomeController.java
+++ BusquedaEmpleos/src/main/java/com/mx/SprigBootUdemy/BusquedaEmpleos/Controladores/HomeController.java
@@ -14,24 +14,29 @@ public class HomeController {
 	
 	@GetMapping("/detalleVacante")
 	public String godetalleVacante(Model model) {
+		ArrayList<Vacante> listVacante=new ArrayList<Vacante>();
 		Vacante vacante=new Vacante();
 		vacante.setId(1);
 		vacante.setNombre("Licenciado en contabilidad");
 		vacante.setDescripcion("Para realizar contabilidad de la empresa");
 		vacante.setSalario(1234.00);
-		model.addAttribute("detalleVacante_detalleVacante",vacante);
+		vacante.setEstatus(true);
+		vacante.setDestacado(false);
+		Vacante vacante1=new Vacante();
+		vacante1.setId(2);
+		vacante1.setNombre("Ingeniero en sistemas");
+		vacante1.setDescripcion("programas");
+		vacante1.setSalario(10000.00);
+		vacante1.setEstatus(false);
+		vacante1.setDestacado(true);
+
+		listVacante.add(vacante);
+		listVacante.add(vacante1);
+		model.addAttribute("detalleVacante_detalleVacante",listVacante);
 		return "detalleVacante";
 	}
 	
-	@GetMapping("/listado")
-	public String golistado(Model model) {
-		ArrayList<String> titulos= new ArrayList<String>();
-		titulos.add("ingeniero en sistemas");
-		titulos.add("ingeniero en medio ambiente");
-		titulos.add("licenciado en ciencias");
-		model.addAttribute("listado_listado",titulos);
-		return "listado";
-	}
+	
 	
 	@GetMapping("/")
 	public String goHome(Model model) {
---
+++ BusquedaEmpleos/src/main/java/com/mx/SprigBootUdemy/BusquedaEmpleos/Modelos/Vacante.java
@@ -0,0 +1,65 @@
+package com.mx.SprigBootUdemy.BusquedaEmpleos.Modelos;
+
+import java.util.Date;
+
+public class Vacante {
+	private int id;
+	private String nombre;
+	private String descripcion;
+	private double salario;
+	private Date fecha;
+	private boolean estatus;
+	private String sexo;
+	private boolean destacado;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public boolean isEstatus() {
+		return estatus;
+	}
+	public void setEstatus(boolean estatus) {
+		this.estatus = estatus;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public boolean isDestacado() {
+		return destacado;
+	}
+	public void setDestacado(boolean destacado) {
+		this.destacado = destacado;
+	}
+	
+	
+}
